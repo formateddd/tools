b6bfc872c7027304d453d727044b9b4f7f808bc9
The handsome boy <17539640+jokernotgod@users.noreply.github.com>
2018-08-09 16:57:12 +0800

    fix bug about when read README.md

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 
 # Get the long description from the README file
-with open('README.md') as file:
+with open('README.md', 'r', encoding='utf8') as file:
     long_description = file.read()
 
 setup(
